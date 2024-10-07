4da2e6f940ecccde326b27aff8a059a793409652
Ayush <ayushrajput000031@gmail.com>
2024-10-07 08:56:40 +0530

    added check odd code

--- checkodd.js
+++ checkodd.js
@@ -1,7 +1,9 @@
-let n = 8;
-if(n % 2 == 0){
-    console.log("Even")
-}
-else{
-    console.log("Odd");
+let array = [2,5,6,8,54,95,3,6,84,65];
+for(i=0;i<array.length;i++){
+    if(array[i]%2==0){
+        console.log(array[i],"is not a odd number");
+    }
+    else{
+        console.log(array[i],"is a odd number");
+    }
 }
