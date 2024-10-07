c332d2df395bc535b5d629f9a4e90410d8626db7
Ayush <ayushrajput000031@gmail.com>
2024-10-07 11:07:17 +0530

    change numbers in array

--- checkodd.js
+++ checkodd.js
@@ -1,4 +1,4 @@
-let array = [2,5,6,8,54,95,3,6,84,65];
+let array = [2,5,6,8,54,95,3,6,84,65,95,52];
 for(i=0;i<array.length;i++){
     if(array[i]%2==0){
         console.log(array[i],"is not a odd number");
